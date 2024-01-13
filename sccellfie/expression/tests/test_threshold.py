33e411b98cab266cf3a39e3ef75c385598df98f6
Erick Armingol <earmingol14@gmail.com>
2024-01-13 13:30:17 +0000

    Improved tests

--- sccellfie/expression/tests/test_threshold.py
+++ sccellfie/expression/tests/test_threshold.py
@@ -2,16 +2,8 @@ import pytest
 import pandas as pd
 
 from pandas.testing import assert_frame_equal
-from sccellfie.expression.thresholds import get_local_mean_threshold, get_global_mean_threshold, get_global_percentile_threshold, get_local_percentile_threshold
+from sccellfie.expression.thresholds import get_local_mean_threshold, get_global_mean_threshold, get_global_percentile_threshold, get_local_percentile_threshold, set_manual_threshold
 from sccellfie.tests.toy_inputs import create_controlled_adata
-
-
-# data = np.array([
-#         [1, 2, 0],  # Cell1
-#         [3, 4, 2],  # Cell2
-#         [5, 6, 10],  # Cell3
-#         [7, 8, 6],  # Cell4
-#     ])
 
 
 @pytest.mark.parametrize("use_raw, lower_bound, upper_bound, exclude_zeros",
@@ -234,3 +226,63 @@ def test_get_global_mean_threshold(use_raw, lower_bound, upper_bound, exclude_ze
         assert all(thresholds >= lower_bound), "Lower bound not respected"
     if upper_bound is not None:
         assert all(thresholds <= upper_bound), "Upper bound not respected"
+
+
+def test_percentile_list():
+    # Create a small, controlled AnnData object
+    adata = create_controlled_adata()
+
+    # Run the function
+    percentile = [0.25, 0.50, 0.75]
+    local_thresholds = get_local_percentile_threshold(adata, percentile=percentile)
+    global_thresholds = get_global_percentile_threshold(adata, percentile=percentile)
+
+    # Expected columns
+    columns = ['threshold-{}'.format(p) for p in percentile]
+
+    # Test output structure
+    assert all(col in local_thresholds.columns for col in columns), "Missing columns"
+    assert all(col in global_thresholds.columns for col in columns), "Missing columns"
+
+
+def test_set_manual_threshold():
+    # Create a small, controlled AnnData object
+    adata = create_controlled_adata()
+
+    # Run the function
+    thresholds = set_manual_threshold(adata, threshold=5)
+
+    # Expected values
+    expected_values = pd.DataFrame({'threshold-manual': [5, 5, 5]},
+                                   index=['gene1', 'gene2', 'gene3'],
+                                   dtype=float)
+
+    # Test output structure
+    assert isinstance(thresholds, pd.DataFrame)
+    assert 'threshold-manual' in thresholds.columns
+    assert thresholds.shape == expected_values.shape
+
+    # Test correctness of values
+    assert thresholds.equals(expected_values), "Threshold values do not match expected results"
+
+
+def test_manual_threshold_per_gene():
+    # Create a small, controlled AnnData object
+    adata = create_controlled_adata()
+
+    # Run the function
+    threshold_list = [5, 10, 15]
+    thresholds = set_manual_threshold(adata, threshold=threshold_list)
+
+    # Expected values
+    expected_values = pd.DataFrame({'threshold-manual': threshold_list},
+                                   index=['gene1', 'gene2', 'gene3'],
+                                   dtype=float)
+
+    # Test output structure
+    assert isinstance(thresholds, pd.DataFrame)
+    assert 'threshold-manual' in thresholds.columns
+    assert thresholds.shape == expected_values.shape
+
+    # Test correctness of values
+    assert thresholds.equals(expected_values), "Threshold values do not match expected results"
--- sccellfie/external/tests/test_tf_idf.py
+++ sccellfie/external/tests/test_tf_idf.py
@@ -10,8 +10,8 @@ def test_output_structure():
 
     expected_columns = ['gene', 'cluster', 'tf', 'idf', 'tf_idf', 'gene_frequency_outside_cluster',
                         'gene_frequency_global', 'second_best_tf', 'second_best_cluster', 'pval', 'qval']
-    assert isinstance(markers, pd.DataFrame)
-    assert all(column in markers.columns for column in expected_columns)
+    assert isinstance(markers, pd.DataFrame), "Output is not a pandas DataFrame"
+    assert all(column in markers.columns for column in expected_columns), "Missing columns in output"
 
 
 def test_number_of_markers():
@@ -38,7 +38,7 @@ def test_expression_cutoff():
     markers1 = quick_markers(adata, cluster_key='cluster', fdr=1.0)
     markers2 = quick_markers(adata, cluster_key='cluster', express_cut=express_cut, fdr=1.0)
 
-    assert ~markers1.equals(markers2)
+    assert ~markers1.equals(markers2), "The two marker sets are the same, expression cutoff not making a difference"
 
 
 def test_quick_markers_r_output_columns():
@@ -48,4 +48,17 @@ def test_quick_markers_r_output_columns():
 
     expected_columns = ['gene', 'cluster', 'geneFrequency', 'geneFrequencyOutsideCluster',
                         'geneFrequencySecondBest', 'geneFrequencyGlobal', 'secondBestClusterName', 'tfidf', 'idf', 'qval']
-    assert all(column in markers.columns for column in expected_columns)
+    assert all(column in markers.columns for column in expected_columns), "Missing columns in output"
+
+
+def test_cell_groups():
+    adata = create_random_adata()
+    markers = quick_markers(adata, cluster_key='cluster', cell_groups=['cluster1', 'cluster2', 'cluster3'], fdr=1.0)
+
+    expected_columns = ['gene', 'cluster', 'tf', 'idf', 'tf_idf', 'gene_frequency_outside_cluster',
+                        'gene_frequency_global', 'second_best_tf', 'second_best_cluster', 'pval', 'qval']
+    assert isinstance(markers, pd.DataFrame), "Output is not a pandas DataFrame"
+    assert all(column in markers.columns for column in expected_columns), "Missing columns in output"
+    assert len(markers['cluster'].unique().tolist()) == len(['cluster1', 'cluster2', 'cluster3']), "Clusters are not properly filtered"
+    assert all(c in markers['cluster'].unique() for c in ['cluster1', 'cluster2', 'cluster3']), "Missing clusters in output"
+    assert all(c not in markers['cluster'].unique() for c in ['cluster4', 'cluster5']), "Missing clusters in output"
