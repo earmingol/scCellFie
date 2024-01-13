4e28d6a652094ca63054415b70d26a181018be50
Erick Armingol <earmingol14@gmail.com>
2024-01-13 13:29:53 +0000

    Removed unused functions

--- sccellfie/__init__.py
+++ sccellfie/__init__.py
@@ -4,6 +4,5 @@ from . import metabolic_task
 from . import reaction_activity
 from . import stats as tl
 from .expression import thresholds, aggregation
-from .utils import gpr_parsing
 
 __version__ = "0.1.1"
