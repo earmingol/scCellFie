d1390b0ff302ab3325e621c4acba0a9bda01b29c
Erick Armingol <earmingol14@gmail.com>
2024-01-17 16:48:31 +0000

    Enabled preprocessing of inputs

--- sccellfie/__init__.py
+++ sccellfie/__init__.py
@@ -1,9 +1,10 @@
 from . import external
 from . import gene_score
 from . import metabolic_task
+from . import preprocessing
 from . import reaction_activity
 from . import spatial
 from . import stats as tl
 from .expression import thresholds, aggregation
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
