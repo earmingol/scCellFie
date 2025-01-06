6f8e0a89c05b542ab2daeb92d1bd16ec40ab4522
Erick Armingol <earmingol14@gmail.com>
2025-01-06 18:35:34 -0300

    Added pairwise test (useful for annotated spatial)

--- sccellfie/stats/__init__.py
+++ sccellfie/stats/__init__.py
@@ -1,2 +1,2 @@
-from .differential_analysis import (cohens_d, scanpy_differential_analysis)
+from .differential_analysis import (cohens_d, scanpy_differential_analysis, pairwise_differential_analysis)
 from .markers_from_task import (get_task_determinant_genes)
