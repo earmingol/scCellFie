d26a6b16db46784c5e72d20fb56edfbfd1704af2
Erick Armingol <earmingol14@gmail.com>
2025-01-24 13:29:52 +0000

    Added GAM imports

--- sccellfie/stats/__init__.py
+++ sccellfie/stats/__init__.py
@@ -1,2 +1,3 @@
 from .differential_analysis import (cohens_d, scanpy_differential_analysis, pairwise_differential_analysis)
+from .gam_analysis import (generate_pseudobulks, fit_gam_model, analyze_gam_results)
 from .markers_from_task import (get_task_determinant_genes)
