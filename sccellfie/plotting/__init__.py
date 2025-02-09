5ac6b312877acee7bf22621951c7519e3c8dc267
Erick Armingol <earmingol14@gmail.com>
2025-02-09 21:36:07 +0000

    Included new plots as imports

--- sccellfie/plotting/__init__.py
+++ sccellfie/plotting/__init__.py
@@ -1,3 +1,4 @@
-from .differential_results import (create_volcano_plot, create_comparative_violin)
+from .communication import (plot_communication_network)
+from .differential_results import (create_volcano_plot, create_comparative_violin, create_beeswarm_plot)
 from .distributions import (create_multi_violin_plots)
 from .spatial import (plot_neighbor_distribution, plot_spatial)
